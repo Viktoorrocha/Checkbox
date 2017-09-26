1ba78165dbf245a227e49dce96559ec7cab1e68e
Viktor Rocha <viktoorrocha@gmail.com>
2017-09-25 21:11:54 -0400

    First

---
+++ app/src/main/java/checkbox/br/com/checkbox/MainActivity.java
@@ -0,0 +1,31 @@
+package checkbox.br.com.checkbox;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CheckBox checkBoxCao;
+    private CheckBox checkBoxGato;
+    private CheckBox checkBoxPapagaio;
+    private Button botaoMostrar;
+    private TextView textoExibicao;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        checkBoxCao = (CheckBox) findViewById(R.id.checkBoxCaoId);
+        checkBoxGato = (CheckBox) findViewById(R.id.checkBoxGatoId);
+        checkBoxPapagaio = (CheckBox) findViewById(R.id.checkBoxPapagaioId);
+
+        textoExibicao = (TextView) findViewById(R.id.textoExibicaoId);
+        botaoMostrar = (Button) findViewById(R.id.botaoMostrarId);
+
+    }
+}
